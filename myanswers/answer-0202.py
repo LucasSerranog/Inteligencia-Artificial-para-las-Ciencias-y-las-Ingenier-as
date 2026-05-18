c7d2e6c8c93aeefa3d6c89a45db8f8eea28933cf
LucasSerranog <lucas.serranog@udea.edu.co>
2026-05-18 18:49:19 -0500

    Refactor inventory rotation calculation logic

--- myanswers/answer-0202.py
+++ myanswers/answer-0202.py
@@ -2,16 +2,15 @@ import pandas as pd
 import numpy as np
 
 def calcular_rotacion_inventario(*args, **kwargs):
-    # Extrae df de donde sea que venga
+    kwargs.pop('output', None)  # ignora el key 'output' si viene
+
     if args:
         arg = args[0]
-        if isinstance(arg, dict):
-            df = arg.get("df") or arg.get("input", {}).get("df")
-        else:
-            df = arg
-    else:
-        inp = kwargs.get("input") or kwargs.get("df") or kwargs
-        df = inp.get("df") if isinstance(inp, dict) else inp
+        df = arg.get("df") if isinstance(arg, dict) else arg
+    elif "input" in kwargs:
+        df = kwargs["input"]["df"]
+    elif "df" in kwargs:
+        df = kwargs["df"]
 
     res = df.copy()
     res['final_stock']   = res['initial_stock'] - res['units_sold'] + res['restock_units']
