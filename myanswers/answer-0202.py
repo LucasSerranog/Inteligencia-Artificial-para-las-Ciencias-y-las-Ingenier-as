46457c7e79bbaa1b6a4e834c978b6a1394d25669
LucasSerranog <lucas.serranog@udea.edu.co>
2026-05-18 18:46:33 -0500

    Refactor calcular_rotacion_inventario to accept args and kwargs

--- myanswers/answer-0202.py
+++ myanswers/answer-0202.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np
 
-def calcular_rotacion_inventario(input):
-    df = input["df"]
+def calcular_rotacion_inventario(*args, **kwargs):
+    # Acepta cualquier forma de llamado
+    if args:
+        arg = args[0]
+        df = arg["df"] if isinstance(arg, dict) else arg
+    elif "input" in kwargs:
+        df = kwargs["input"]["df"]
+    elif "df" in kwargs:
+        df = kwargs["df"]
+
     res = df.copy()
     res['final_stock']   = res['initial_stock'] - res['units_sold'] + res['restock_units']
     avg_stock            = (res['initial_stock'] + res['final_stock']) / 2
