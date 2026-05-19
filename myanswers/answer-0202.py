9fa4b9089addb1799537ab016812970ad7ab0a60
LucasSerranog <lucas.serranog@udea.edu.co>
2026-05-18 19:06:10 -0500

    Refactor calcular_rotacion_inventario function

--- myanswers/answer-0202.py
+++ myanswers/answer-0202.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def calcular_rotacion_inventario(df=None, input=None, output=None):
-    if df is None and input is not None:
-        df = input["df"]
+def calcular_rotacion_inventario(df):
     df = df.copy()
     df['final_stock'] = df['initial_stock'] - df['units_sold'] + df['restock_units']
     avg_stock = (df['initial_stock'] + df['final_stock']) / 2
